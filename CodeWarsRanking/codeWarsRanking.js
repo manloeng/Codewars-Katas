8698019c3c9f75ad1aa631e9a1303123ef782e3a
Andrew <manloengchung@yahoo.co.uk>
2020-01-05 23:07:44 +0000

    Cleanest solution for this kata

--- CodeWarsRanking/codeWarsRanking.js
+++ CodeWarsRanking/codeWarsRanking.js
@@ -1,11 +1,43 @@
-class User {
-  constructor() {
-    this.incProgress();
-  }
+const RANKS = Object.freeze([-8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8])
+const MIN_RANK = RANKS[0]
+const MAX_RANK = RANKS[RANKS.length -1]
+const POINTS_PER_RANK = 100
 
-  incProgress() {
-    this.rank = -8
-  }
+class User {
+    constructor() {
+        this.rank = MIN_RANK
+        this.progress = 0
+    }
+
+    incProgress(taskRank) {
+        if (!RANKS.includes(taskRank)) throw new Error()
+        if (this.rank === MAX_RANK) return
+
+        const userPosition = RANKS.indexOf(this.rank)
+        const taskPosition = RANKS.indexOf(taskRank)
+
+        const points = calculatePoints(userPosition, taskPosition)
+        const totalPoints = points + this.progress
+
+        const rankIncrease = Math.floor(totalPoints / POINTS_PER_RANK)
+        const newPosition = userPosition + rankIncrease
+        this.rank = RANKS[newPosition]
+
+        if (this.rank === MAX_RANK) {
+            this.progress = 0
+        } else {
+            const updatedProgress = totalPoints % POINTS_PER_RANK
+            this.progress = updatedProgress
+        }
+    }
+}
+
+const calculatePoints = (userPosition, taskPosition) => {
+    const rankDifference = taskPosition - userPosition
+    if (rankDifference === 0) return 3
+    if (rankDifference === -1) return 1
+    if (rankDifference <= -2) return 0
+    return 10 * rankDifference * rankDifference
 }
 
 const user = new User()
