8322c9f6bb8fd0df42fcf64e1296097905e76a27
Andrew <manloengchung@yahoo.co.uk>
2020-01-11 23:27:07 +0000

    Refactor if statement

--- RangeExtractor/rangeExtractor.js
+++ RangeExtractor/rangeExtractor.js
@@ -2,7 +2,6 @@ function rangeExtractor(list) {
   if (!list.length) return "";
   if (list.length === 1 || list.length === 2) return list.join().toString();
   const newList = getNewRangeList(list);
-  console.log(newList);
   return newList.join(",").toString();
 }
 
@@ -14,6 +13,22 @@ function isNextNumberAnIncreament(currentNumber, nextNumber) {
 
 function increamentCurrentNumber(currentNumber) {
   return currentNumber + 1;
+}
+
+function shouldIncrementOnce(countIncreament) {
+  return countIncreament === 1;
+}
+
+function shouldIncrementMultipleTimes(countIncreament) {
+  return countIncreament > 1;
+}
+
+function hasStartEndAndIncrementOnce(startNumber, endNumber, countIncreament) {
+  return startNumber && endNumber && countIncreament === 1;
+}
+
+function hasStartEnd(startNumber, endNumber) {
+  return startNumber && endNumber;
 }
 
 function getNewRangeList(list) {
@@ -28,21 +43,22 @@ function getNewRangeList(list) {
 
     if (isNextNumberAnIncreament(currentNumber, nextNumber)) {
       countIncreament++;
-      
-      if (countIncreament === 1) {
+
+      if (shouldIncrementOnce(countIncreament)) {
         startNumber = currentNumber;
         endNumber = nextNumber;
       }
 
-      if (countIncreament > 1) endNumber = nextNumber;
-
-    } else if (startNumber && endNumber && countIncreament === 1) {
+      if (shouldIncrementMultipleTimes(countIncreament)) endNumber = nextNumber;
+    } else if (
+      hasStartEndAndIncrementOnce(startNumber, endNumber, countIncreament)
+    ) {
       newList.push(startNumber);
       newList.push(endNumber);
       countIncreament = 0;
       startNumber = 0;
       endNumber = 0;
-    } else if (startNumber && endNumber) {
+    } else if (hasStartEnd(startNumber, endNumber)) {
       const range = `${startNumber}-${endNumber}`;
       newList.push(range);
       countIncreament = 0;
